696298febe1bed69de616eca5d06e2245ab9a016
Bruno Petrolini <bruno.petrolini@contasimples.com>
2024-01-15 12:04:57 -0300

    feat: adds database connection

---
+++ api/src/database/database.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	"database/sql"
+	"devbook/src/config"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// Connect opens a connection to the database
+func Connect() (*sql.DB, error) {
+	db, error := sql.Open("mysql", config.ConnectionString)
+	if error != nil {
+		return nil, error
+	}
+
+	if error = db.Ping(); error != nil {
+		db.Close()
+		return nil, error
+	}
+
+	return db, nil
+}
