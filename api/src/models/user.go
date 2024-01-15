756f7d711b8d48b0085f9fd5807ec01a19199909
Bruno Petrolini <bruno.petrolini@contasimples.com>
2024-01-15 11:12:43 -0300

    feat: adds user model

---
+++ api/src/models/user.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+// User represents a user in the social network
+type User struct {
+	ID        uint64    `json:"id,omitempty"`
+	Name      string    `json:"name,omitempty"`
+	Nickname  string    `json:"nickname,omitempty"`
+	Email     string    `json:"email,omitempty"`
+	Password  string    `json:"password,omitempty"`
+	CreatedAt time.Time `json:"created_at,omitempty"`
+}
